b7c4312a298e344ff8be4b07d06254db2f4e906e
etechas <etechas@LI08-13>
2024-03-25 22:04:19 -0300

    Add project files.

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            ListView listView = new ListView();
+            listView.SetBinding(ItemsView.ItemsSourceProperty, "Number");
+
+        }
+    }
+
+}
